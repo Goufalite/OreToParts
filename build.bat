d133bc3f448095aec4bd9b964103c70fcc708720
Goufalite <36901323+Goufalite@users.noreply.github.com>
2021-07-08 22:40:19 +0200

    Build tweaks

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
-xcopy /f /y /r %1 "GameData\OreToParts\Plugins\"
+xcopy /f /y /r %1 "..\..\GameData\OreToParts\Plugins\"
 
 xcopy /f /y /r %1 "C:\Jeux\ksp\ksp-1-11-sandbox\GameData\OreToParts\Plugins\"
+
+xcopy /f /y /r %1 "C:\Jeux\ksp\ksp-1-12-1-sandbox\GameData\OreToParts\Plugins\"
